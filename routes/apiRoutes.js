2e8f67a3d80172dc21203caddb5ce86cb3153b00
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-02-21 17:47:52 -0500

    add delete notes function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -41,4 +41,31 @@ router.post("/notes", function(req, res) {
     })
 });
 
+router.delete("/notes/:id", function(req, res) {
+    const id = parseInt(req.params.id);
+
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        const notes = JSON.parse(data);
+
+        let index = notes.findIndex(note => note.id == id);
+        if(index != -1){
+            notes.splice(index, 1);
+        } else {
+            return;
+        }
+  
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes, null, 2), (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Note removed.");
+            res.json(req.body);
+        });
+    })
+        
+});
+
 module.exports  = router;
